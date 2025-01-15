81301b00e8b8262c2db14e330523f45476dd2a37
urclown <yourclown31@gmail.com>
2025-01-15 20:37:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('revealButton').addEventListener('click', function() {
     document.getElementById('hiddenMessage').classList.remove('hidden');
 });
 document.getElementById('acceptButton').addEventListener('click', function() {
-    alert('lovee uu, meskipun kamuu cewe, orang tapi kalo jodohnya akuu yaa punya akuu.');
+    alert('lovee uu, meskipun kamuu cewe orang. tapi kalo jodohnya akuu yaa punya akuu.');
 });
